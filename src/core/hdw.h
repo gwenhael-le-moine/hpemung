92e96f5ef88f391820c293130e381237df91f4d8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2025-12-09 10:40:58 +0100

    Codeberg Migration
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

--- src/core/hdw.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HDW_H
-#define __HDW_H
-
-#include "types.h"
-
-extern byte hdw_read_nibble( address adr );
-extern void hdw_write_nibble( byte data, address adr );
-
-#endif
--- src/core/rpl.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RPL_H
-#define __RPL_H
-
-#include "types.h"
-
-extern int rpl_object_size( byte* obj );
-extern int rpl_push_object( byte* obj, address size );
-
-#endif
